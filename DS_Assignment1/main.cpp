85a83c62a1964c7a6d89685b88c89871542dfa20
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2015-12-06 20:20:45 +0800

    Solving some linking issue

---
+++ DS_Assignment1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "Dweller.h"
+#include "Outfit.h"
+#include "Weapon.h"
+#include "Item.h"
+#include "GameObject.h"
+#include "Vec2D.h"
+
+void main()
+{
+	cout << "hi" << endl;
+}
